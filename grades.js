35ea547ea41a2311a2a05800454d23b20ebb2ae7
AlizaJamshed <160414263+AlizaJamshed@users.noreply.github.com>
2024-03-06 21:53:54 -0800

    Add files via upload

---
+++ grades.js
@@ -0,0 +1,19 @@
+var percentage = 95;
+if (percentage > 90) {
+    console.log("You got A1 Grade");
+}
+else if (percentage < 90 && percentage > 80) {
+    console.log("You Got A Grade");
+}
+else if (percentage < 80 && percentage > 60) {
+    console.log("You Got B Grade");
+}
+else if (percentage < 60 && percentage > 50) {
+    console.log("You Got C Grade");
+}
+else if (percentage < 50 && percentage > 40) {
+    console.log("You Got D Grade");
+}
+else {
+    console.log("You Are Fail");
+}
